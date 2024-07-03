fd7e6569a3ccfc8ee67cc68d3425dfd3e39dd8f7
LRJerome <asterix.1976@hotmail.fr>
2024-07-03 16:18:32 +0200

    sprint7_ticket1_contact_feat_done

--- migrations/Version20240703123638.php
+++ migrations/Version20240703123638.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240628062403 extends AbstractMigration
+final class Version20240703123638 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240628062403 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE category CHANGE images images JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE contact CHANGE created_at created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE category CHANGE images images LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE contact CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 }
